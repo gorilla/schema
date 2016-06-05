14db15a604ba48b9fdd06e66ae8e219e4a49117d
mayowa <mayowa@gmail.com>
2016-06-05 12:15:45 +0100

    UnmarshalText prioritized for slices

--- decoder.go
+++ decoder.go
@@ -143,6 +143,7 @@ func isEmpty(t reflect.Type, value []string) bool {
 
 // decode fills a struct field using a parsed path.
 func (d *Decoder) decode(v reflect.Value, path string, parts []pathPart, values []string) error {
+
 	// Get the field walking the struct fields by index.
 	for _, name := range parts[0].path {
 		if v.Type().Kind() == reflect.Ptr {
@@ -153,7 +154,6 @@ func (d *Decoder) decode(v reflect.Value, path string, parts []pathPart, values 
 		}
 		v = v.FieldByName(name)
 	}
-
 	// Don't even bother for unexported fields.
 	if !v.CanSet() {
 		return nil
@@ -188,6 +188,7 @@ func (d *Decoder) decode(v reflect.Value, path string, parts []pathPart, values 
 	if conv == nil && t.Kind() == reflect.Slice {
 		var items []reflect.Value
 		elemT := t.Elem()
+		//elemP := elemT
 		isPtrElem := elemT.Kind() == reflect.Ptr
 		if isPtrElem {
 			elemT = elemT.Elem()
@@ -205,6 +206,21 @@ func (d *Decoder) decode(v reflect.Value, path string, parts []pathPart, values 
 			if value == "" {
 				if d.zeroEmpty {
 					items = append(items, reflect.Zero(elemT))
+				}
+			} else if _, ok := isTextUnmarshaler(v); ok {
+				u := reflect.New(elemT)
+				if err := u.Interface().(encoding.TextUnmarshaler).UnmarshalText([]byte(value)); err != nil {
+					return ConversionError{
+						Key:   path,
+						Type:  t,
+						Index: key,
+						Err:   err,
+					}
+				}
+				if u.Kind() == reflect.Ptr {
+					items = append(items, u.Elem())
+				} else {
+					items = append(items, u)
 				}
 			} else if item := conv(value); item.IsValid() {
 				if isPtrElem {
@@ -298,6 +314,20 @@ func isTextUnmarshaler(v reflect.Value) (encoding.TextUnmarshaler, bool) {
 	if v.CanAddr() {
 		v = v.Addr()
 	}
+	if u, ok := v.Interface().(encoding.TextUnmarshaler); ok {
+		return u, ok
+	}
+
+	// if v is []T or *[]T create new T
+	t := v.Type()
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+	if t.Kind() == reflect.Slice {
+		t = t.Elem()
+	}
+
+	v = reflect.New(t)
 	u, ok := v.Interface().(encoding.TextUnmarshaler)
 	return u, ok
 }

f73dd38aa2f6b247ba1a29b12c2e62b20e0b09d6
kucherenkovova <kucherenkovova@gmail.com>
2019-09-01 21:17:54 +0300

    added optimization if key is not in dot notation

--- decoder.go
+++ decoder.go
@@ -152,6 +152,10 @@ type fieldWithPrefix struct {
 func isEmptyFields(fields []fieldWithPrefix, src map[string][]string) bool {
 	for _, f := range fields {
 		for _, path := range f.paths(f.prefix) {
+			v, ok := src[path]
+			if ok && !isEmpty(f.typ, v) {
+				return false
+			}
 			for key := range src {
 				if !isEmpty(f.typ, src[key]) && strings.HasPrefix(key, path) {
 					return false
